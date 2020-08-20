c50ad52b8ddc201cfbbc11a63089df3f4f1dab62
andrewtempany <andrewtempany95@gmail.com>
2020-08-20 13:30:14 +1200

    add globalstyles.js

--- App.js
+++ App.js
@@ -1,22 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import GlobalStyles from "./GlobalStyles";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  Alert,
+  TouchableNativeFeedback,
+  View,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
-  let x = 1;
+  const handlePress = () => console.log("It has been pressed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={GlobalStyles.basic}>
       <StatusBar style="auto" />
-    </View>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Open up App.js
+      </Text>
+      <TouchableNativeFeedback
+        onPress={() =>
+          Alert.alert("title", "message", [
+            { text: "yes", onPress: () => console.log("yes") },
+            { text: "no", onPress: () => console.log("no") },
+          ])
+        }
+      >
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "pink" }}
+        ></View>
+      </TouchableNativeFeedback>
+      <Image
+        // blurRadius={1}
+        fadeDuration={2000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/300",
+        }}
+      />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ GlobalStyles.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Platform, StatusBar } from "react-native";
+export default StyleSheet.create({
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: "blue",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    alignItems: "center",
+  },
+  basic: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
+  
