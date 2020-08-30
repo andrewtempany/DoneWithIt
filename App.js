adf14f78a72d398899c1563ea668dfb115c9e25e
andrewtempany <andrewtempany95@gmail.com>
2020-08-31 09:37:20 +1200

    create listing details screen

--- App.js
+++ App.js
@@ -1,29 +1,36 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import AppText from "./app/components/AppText";
-import Colours from "./app/config/colours";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import AppButton from "./app/components/AppButton";
-import WelcomeScreen from "./app/Screens/WelcomeScreen";
-import Card from "./app/components/Card";
-import ListingDetails from "./app/Screens/ListingDetails";
+import { View, Text, StyleSheet } from "react-native";
+import ListingDetailsScreen from "./app/Screens/ListingDetailsScreen";
+
+// import AppText from "./app/components/AppText";
+// import Colours from "./app/config/colours";
+// import { MaterialCommunityIcons } from "@expo/vector-icons";
+// import AppButton from "./app/components/AppButton";
+// import WelcomeScreen from "./app/Screens/WelcomeScreen";
+// import Card from "./app/components/Card";
 
 // import WelcomeScreen from "./app/Screens/WelcomeScreen";
 // import ViewImageScreen from "./app/Screens/ViewImageScreen"
 
 export default function App() {
   return (
-    <ListingDetails
-      Title="Red Jacket For Sale"
-      Image={require("./app/assets/jacket.jpg")}
-      Subtitle="$100"
-      Author="Mosh Hamedani"
-    />
+    <View style={Styles.container}>
+      <ListingDetailsScreen
+        image={require("./app/assets/jacket.jpg")}
+        title="Red Jacket For Sale"
+        subtitle="$100"
+        profilePic={require("./app/assets/mosh.jpg")}
+        profileName="Mosh Hamedani"
+        listings="5 Listings"
+      />
+    </View>
   );
 }
 
-// const Styles = StyleSheet.create({
-//   container: {
-
-//   },
-// });
+const Styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: "dodgerblue",
+  },
+});
---
+++ app/Screens/ListingDetailsScreen.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { View, StyleSheet, Image, Text } from "react-native";
+import colours from "../config/colours";
+
+function ListingDetailsScreen({
+  image,
+  title,
+  subtitle,
+  profilePic,
+  profileName,
+  listings,
+}) {
+  return (
+    <View style={Styles.container}>
+      <Image source={image} style={Styles.image}></Image>
+      <View style={Styles.textContainer}>
+        <Text style={Styles.title}>{title}</Text>
+        <Text style={Styles.subtitle}>{subtitle}</Text>
+      </View>
+      <View style={Styles.profileContainer}>
+        <Image source={profilePic} style={Styles.profilePic}></Image>
+        <View style={Styles.profileTextContainer}>
+          <Text>{profileName}</Text>
+          <Text style={Styles.profileSubtext}>{listings}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colours.white,
+  },
+  image: {
+    maxHeight: "35%",
+    maxWidth: "100%",
+  },
+  profileContainer: {
+    flexDirection: "row",
+    marginLeft: "7.5%",
+  },
+  profilePic: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+  },
+  profileTextContainer: {
+    marginLeft: 20,
+    paddingTop: 0,
+    height: 60,
+    justifyContent: "center",
+  },
+  profileSubtext: {
+    color: "grey",
+  },
+  subtitle: {
+    color: colours.secondary,
+    marginLeft: "5%",
+    marginTop: 5,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  textContainer: {
+    margin: "7.5%",
+  },
+  title: {
+    fontFamily: "Roboto",
+    fontWeight: "400",
+    marginLeft: "5%",
+    fontSize: 16,
+  },
+});
+
+export default ListingDetailsScreen;
